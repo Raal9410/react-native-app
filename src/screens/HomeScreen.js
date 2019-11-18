4673c3ec559d0dc5749ea1d96491365ce354410d
Raal9410 <rahm9410@gmail.com>
2019-11-18 15:48:25 -0600

    buttons on HomeScreen component

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,17 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View, Button } from 'react-native';
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>Hi there!!</Text>;
+  return (
+    <View>
+      <Text style={styles.text}>Hi there!!</Text>
+      <Button
+      onPress={() => console.log('Button pressed')} 
+      title='Go to Components Demo'
+      />
+      <Button title='Go to List Demo'/>
+    </View>
+  )
 };
 
 const styles = StyleSheet.create({
